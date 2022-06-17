34f8e186a1a6af21a9a9ae896c4096b7ede19908
aselwilliams <aselwilliams22@gmail.com>
2022-06-17 17:35:54 -0400

    markup added

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+    <div class="row">
+        <h1 class="title">CATS-LAND
+            <img src="https://images.pexels.com/photos/45170/kittens-cat-cat-puppy-rush-45170.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="kittens" />
+        </h1>
+    </div>
+    <div>
+        <p id="alert-msg">Some Alert</p>
+    </div>
+</header>
+  )
+}
+
+export default Header
